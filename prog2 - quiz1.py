87bec4ae5a0ffa84c6c217e1c68f6f301910d8d1
rauladolfotecgda <rauladolfo.tecgda@gmail.com>
2016-02-18 20:45:54 -0600

    These are all program of all quizzes of the First Partial

---
+++ prog2 - quiz1.py
@@ -0,0 +1,14 @@
+#Raúl Adolfo Torres Vargas
+#A01400187
+#Quiz #1, program 2
+
+a = int(input("Please give me the first number: "))
+b = int(input("Please give me the second number: "))
+
+product = int(a * b)
+division = int (a / b)
+remainder = int(a % b)
+
+print(product)
+print(division)
+print(remainder)
